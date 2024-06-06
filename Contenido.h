0824e132ac2176c8f93bee05043f1db17dd9c1f5
Elmay05 <150318195+Elmay05@users.noreply.github.com>
2024-06-06 16:51:37 -0600

    Contenido.h

--- Contenido.h
+++ Contenido.h
@@ -1,13 +1,27 @@
 #ifndef CONTENIDO_H_INCLUDED
 #define CONTENIDO_H_INCLUDED
+
 #include <iostream>
 #include <string>
 #include <sstream>
+/*
+ *Proyecto Miavie clase Contenido
+ *Omar Emilio Casillas Alday
+ *A01712114
+ *07/06/2024
+ *Esta clase definida por el tipo Contenido que contienen las clases
+ *heredadas Pelicula y Serie
+ */
 
-// Declaracion de clase Madre
+
+
+/*Declaracion de clase Madre "Contenido" que es abstracta y
+ * heredará a las clases "Pelicula" y "Serie"
+ */
 class Contenido{
     protected:
-        //Declaro variables de instancia
+        //Declaro variables privadas de instancia
+        std::string tipo;
         std::string nombre;
         std::string sinopsis;
         std::string clasificacion;
@@ -15,25 +29,38 @@ class Contenido{
         int categoria;
     public:
         //Declaracion de los constructores
-        Contenido():nombre(""),sinopsis(""),clasificacion(""),categoria(0){};
-        Contenido(std::string nom,std::string sino,std::string cla):nombre(nom),sinopsis(sino),clasificacion(cla),categoria(0){};
+        Contenido():nombre(""),sinopsis(""),clasificacion(""),categoria(0),tipo(""){};
+        Contenido(std::string nom,std::string sino,std::string cla,std::string tip):nombre(nom),sinopsis(sino),clasificacion(cla),categoria(0),tipo(tip){};
         //Declaracion los metodos que va a tener el objeto
+            //getters
         std::string get_nombre(){return nombre;}
         std::string get_sinopsis(){return sinopsis;}
         std::string get_clasificacion(){return clasificacion;}
+        std::string get_tipo(){return tipo;}
+            //setters
         void muestra_categoria();
         void set_nombre(std::string);
         void set_sinopsis(std::string);
         void set_clasificacion(std::string);
         void agregar_categoria(std::string);
+        void set_tipo(std::string);
+        // Metodos abstractos que serán sobreescritos
         virtual std::string resumen();
+        virtual void agrega_temporada(std::string){}
+        virtual void agrega_episodios(int, int){}
+        virtual int get_temporada(){return 0;}
+        virtual ~Contenido(){}
 };
+
+
 void Contenido::set_nombre(std::string nom){
     nombre=nom;
 };
+
 void Contenido::set_sinopsis(std::string sino){
     sinopsis=sino;
 };
+
 void Contenido::set_clasificacion(std::string clas){
     clasificacion=clas;
 };
@@ -42,193 +69,34 @@ void Contenido::agregar_categoria(std::string cat){
     categorias[categoria]=cat;
     categoria++;
 };
+
+void Contenido::set_tipo(std::string tip){
+    tipo=tip;
+};
+/*
+ * muestra_categoria muestra las categorias que
+ * tienen las peliculas o series
+ */
 void Contenido::muestra_categoria(){
     for(int i=0; i<=categoria; i++){
         std::cout<<i+1<<".- "<<categorias[i]<<std::endl;
     }
-}
+};
+/*
+ * resumen muestra todas las partes de la pelicula o serie que
+ * se va a pedir
+ */
+
 std::string Contenido::resumen(){
     std::stringstream aux;
 
-    aux<<"Nombre: "<<nombre<<std::endl<<"Sinopsis: "<<sinopsis<<std::endl<<"Clasificacion: "<<clasificacion<<std::endl<<"Categorias: "<<std::endl;
+    aux<<"Tipo: "<<tipo<<std::endl<<"Nombre: "<<nombre<<std::endl<<"Sinopsis: "<<sinopsis<<std::endl<<"Clasificacion: "<<clasificacion<<std::endl<<"Categorias: "<<std::endl;
     for(int i=0; i<categoria; i++){
         aux<<i+1<<".- "<<categorias[i]<<std::endl;
     }
     return aux.str();
 };
-
-class Pelicula:public Contenido{
-    private:
-        std::string duracion;
-    public:
-        Pelicula():Contenido(),duracion("00:00:00"){};
-        Pelicula(std::string nom,std::string sino,std::string cla,std::string dur):Contenido(nom,sino,cla),duracion(dur){};
-        std::string get_duracion();
-        void set_duracion(std::string);
-        std::string resumen();
-};
-std::string Pelicula::get_duracion(){
-    return duracion;
-};
-void Pelicula::set_duracion(std::string dur){
-    duracion=dur;
-};
-std::string Pelicula::resumen(){
-    std::stringstream aux;
-    aux<<"Nombre: "<<nombre<<std::endl<<"Sinopsis: "<<sinopsis<<std::endl<<"Clasificacion: "<<clasificacion<<std::endl<<"Categorias: "<<std::endl;
-    for(int i=0; i<categoria; i++){
-        aux<<i+1<<".- "<<categorias[i]<<std::endl;
-    }
-    aux<<"Duracion: "<<duracion<<std::endl<<std::endl;
-    return aux.str();
-};
-class Serie:public Contenido{
-    private:
-        std::string temporadas[100];
-        int temporada;
-        int episodios[100];
-
-    public:
-        Serie():Contenido(),temporada(0){};
-        Serie(std::string nom,std::string sino,std::string cla):Contenido(nom,sino,cla),temporada(0){};
-        int get_temporada(){return temporada;}
-        void agrega_temporada(std::string);
-        void agrega_episodios(int, int);
-        std::string resumen();
-
-
-};
-void Serie::agrega_temporada(std::string tem){
-    temporadas[temporada]=tem;
-    temporada++;
-};
-void Serie::agrega_episodios(int tem, int ep){
-    if (temporada==0){
-        std::cout<<"Primero tienes que agregar una temporada"<<std::endl;
-    }
-    else{
-        episodios[tem]=ep;
-    }
-
-};
-std::string Serie::resumen(){
-    std::stringstream aux;
-    aux<<"Nombre: "<<nombre<<std::endl<<"Sinopsis: "<<sinopsis<<std::endl<<"Clasificacion: "<<clasificacion<<std::endl<<"Categorias: "<<std::endl;
-    for(int i=0; i<categoria; i++){
-        aux<<i+1<<".- "<<categorias[i]<<std::endl;
-    }
-    aux<<std::endl;
-    for(int i=0;i<temporada;i++){
-        aux<<"Temporada "<<i+1<<": "<<temporadas[i]<<std::endl;
-        aux<<"Episodios: "<<episodios[i]<<std::endl;
-    }
-    aux<<std::endl;
-    return aux.str();
-};
-
-class MIAvie{
-    private:
-        Serie *series[1000];
-        Pelicula *peliculas[1000];
-        int serie;
-        int pelicula;
-    public:
-        MIAvie():serie(0),pelicula(0){};
-        void agrega_serie(std::string,std::string,std::string);
-        void agrega_pelicula(std::string,std::string,std::string,std::string);
-        void mostrar_series();
-        void mostrar_peliculas();
-        void consulta_serie(int);
-        void consulta_pelicula(int);
-        void agrega_categoria_peli(int, std::string);
-        void agrega_categoria_serie(int, std::string);
-        void agrega_temporada(int,std::string);
-        void agrega_episodio(int,int,int);
-};
-
-void MIAvie::agrega_serie(std::string nom,std::string sino,std::string cla){
-    series[serie]= new Serie(nom,sino,cla);
-    serie++;
-};
-
-void MIAvie::agrega_pelicula(std::string nom,std::string sino,std::string cla,std::string dur){
-    peliculas[pelicula]=new Pelicula (nom,sino,cla,dur);
-    pelicula++;
-};
-
-void MIAvie::mostrar_series(){
-    std::cout<<"Series:"<<std::endl;
-    for(int i=0;i<serie;i++){
-        std::cout<<i+1<<".- "<<series[i]->get_nombre()<<std::endl;
-    }
-
-};
-
-void MIAvie::mostrar_peliculas(){
-    std::cout<<"Peliculas:"<<std::endl;
-    for(int i=0;i<pelicula;i++){
-        std::cout<<i+1<<".- "<<peliculas[i]->get_nombre()<<std::endl;
-    }
-};
-
-void MIAvie::consulta_serie(int ser){
-    if(ser<serie){
-        std::cout<<series[ser]->resumen();
-    }
-    else{
-        std::cout<<"Numero invalido o no existe la serie"<<std::endl;
-    }
-};
-void MIAvie::consulta_pelicula(int pel){
-    if(pel<pelicula){
-        std::cout<<peliculas[pel]->resumen();
-    }
-    else{
-        std::cout<<"Numero invalido o no existe la pelicula"<<std::endl;
-    }
-};
-
-void MIAvie::agrega_categoria_peli(int pel,std::string cat){
-    if(pel-1<pelicula){
-        peliculas[pel-1]->agregar_categoria(cat);
-    }
-    else{
-        std::cout<<"Numero invalido"<<std::endl;
-    }
-};
-void MIAvie::agrega_categoria_serie(int ser,std::string cat){
-    if(ser-1<serie){
-        series[ser-1]->agregar_categoria(cat);
-    }
-    else{
-        std::cout<<"Numero invalido"<<std::endl;
-    }
-};
-
-void MIAvie::agrega_temporada(int ser,std::string tem){
-    if(ser-1<serie){
-        series[ser-1]->agrega_temporada(tem);
-    }else{
-        std::cout<<"No existe serie para ese numero"<<std::endl;
-    }
-
-};
-
-void MIAvie::agrega_episodio(int ser, int tem, int ep){
-    if(ser<serie){
-        if(tem<series[ser]->get_temporada()){
-            series[ser]->agrega_episodios(tem,ep);
-        }else{
-            std::cout<<"No existe temporada"<<std::endl;
-        }
-
-    }
-    else{
-        std::cout<<"No existe serie para ese numero"<<std::endl;
-    }
-};
-
-
 
 
 #endif // CONTENIDO_H_INCLUDED
+
